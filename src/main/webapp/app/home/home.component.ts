fb4f968c3509dc7292e3d7140eeb74fa6b9b6498
LAXMAN <laxman.chari@emirates.com>
2018-01-21 10:33:02 +0400

    adding ckeditor

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -15,6 +15,7 @@ import { Account, LoginModalService, Principal } from '../shared';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    ckeditorContent: string;
 
     constructor(
         private principal: Principal,
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -1,14 +1,19 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 import { CkeditorSharedModule } from '../shared';
-
 import { HOME_ROUTE, HomeComponent } from './';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CKEditorModule } from 'ng2-ckeditor';
+
 
 @NgModule({
     imports: [
         CkeditorSharedModule,
-        RouterModule.forRoot([ HOME_ROUTE ], { useHash: true })
+        RouterModule.forRoot([ HOME_ROUTE ], { useHash: true }),
+        BrowserModule, 
+        CKEditorModule, 
+        FormsModule
     ],
     declarations: [
         HomeComponent,
